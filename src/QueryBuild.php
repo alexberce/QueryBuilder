0ccf0bbd1ac19bfbf3dc5177ee0e2e040eba5947
Adrian Dumitru <adrian.dumitru.68@gmail.com>
2017-08-12 09:20:13 +0300

    Reformat code

--- src/QueryBuild.php
+++ src/QueryBuild.php
@@ -20,51 +20,51 @@ class QueryBuild
     /**
      * @var integer
      */
-	private $queryType;
+    private $queryType;
 
-	/**
-	 * QueryBuild constructor.
-	 * @param $queryType
-	 */
-	protected function __construct( $queryType )
-	{
-		$this->queryType = $queryType;
-	}
+    /**
+     * QueryBuild constructor.
+     * @param $queryType
+     */
+    protected function __construct($queryType)
+    {
+        $this->queryType = $queryType;
+    }
 
-	/**
-	 * @param $table
-	 * @return QuerySelect
-	 */
-	public static function select( $table )
-	{
-		return new QuerySelect( new QueryBuild(0), $table );
-	}
+    /**
+     * @param $table
+     * @return QuerySelect
+     */
+    public static function select($table)
+    {
+        return new QuerySelect(new QueryBuild(0), $table);
+    }
 
-	/**
-	 * @param $table
-	 * @return QueryUpdate
-	 */
-	public static function update( $table )
-	{
-		return new QueryUpdate( new QueryBuild(0), $table );
-	}
+    /**
+     * @param $table
+     * @return QueryUpdate
+     */
+    public static function update($table)
+    {
+        return new QueryUpdate(new QueryBuild(0), $table);
+    }
 
     /**
      * @param $table
      * @return QueryInsert
      */
-    public static function insert( $table )
+    public static function insert($table)
     {
-        return new QueryInsert( new QueryBuild(0), $table );
+        return new QueryInsert(new QueryBuild(0), $table);
     }
 
     /**
      * @param $table
      * @return QueryDelete
      */
-    public static function delete( $table )
+    public static function delete($table)
     {
-        return new QueryDelete( new QueryBuild(0), $table);
+        return new QueryDelete(new QueryBuild(0), $table);
     }
 
     /**
@@ -72,15 +72,15 @@ class QueryBuild
      */
     public static function custom()
     {
-        return new QueryCustom( new QueryBuild(1));
+        return new QueryCustom(new QueryBuild(1));
     }
 
-	/**
-	 * @return integer
-	 */
-	public function getType()
-	{
-		return $this->queryType;
-	}
+    /**
+     * @return integer
+     */
+    public function getType()
+    {
+        return $this->queryType;
+    }
 
 }
