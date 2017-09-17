08f022c4cc2816d7fed5f92ea8957fee80008263
Adrian Dumitru <adrian.dumitru.68@gmail.com>
2017-09-17 03:43:33 +0300

    Add composer.json

--- sample/select.php
+++ sample/select.php
@@ -6,16 +6,26 @@
  * Time: 12:26 PM
  */
 
-include_once 'autoloader.php';
+//include_once 'autoloader.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
+var_dump($_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php');
+var_dump(__DIR__);
 use Qpdb\QueryBuilder\QueryBuild;
 
 
 
 $sql = QueryBuild::select('employees')
-	->whereLike('firstName', '%arry%');
+	->fields("employees.*, offices.city, offices.country")
+	->innerJoin('offices', 'employees.officeCode', 'offices.officeCode')
+	//->groupBy('country DESC')
+	;
 
-echo "<pre>" . print_r($sql->getSyntax(), 1) . "</pre>";
-echo "<pre>" . print_r($sql->getBindParams(), 1) . "</pre>";
-echo "<pre>" . print_r($sql->execute(), 1) . "</pre>";
+//$sql = QueryBuild::select('employees')
+//	->whereLike('lastName','%bo%')
+//;
 
-var_dump(phpversion());
+echo "<pre>" . print_r($sql->getSyntax(),1) . "</pre>";
+echo "<pre>" . print_r($sql->getBindParams(),1) . "</pre>";
+echo "<pre>" . print_r($sql->execute(),1) . "</pre>";
+
+
